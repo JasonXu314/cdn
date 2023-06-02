b4f216e3b4b78de25b32e2a1b50fc09ed5e3607c
Jason Xu <Xinwen31415@gmail.com>
2023-06-02 09:18:08 -0500

    oops

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -86,7 +86,7 @@ export class AppController {
 							Query
 							<input id="search" type="text" oninput="debouncedSearch()" onchange="search()">
 						</label>
-						<label />
+						<label></label>
 						<label for="field">
 							Search By
 							<select id="field" onchange="search()" required>
